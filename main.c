95512ce51aa8ba2f971bbdbf4bfcb889c317e95a
moussanho <moussanho160diop@gmail.com>
2018-04-30 16:39:10 +0200

    implémentation de la somme avec decalage

--- main.c
+++ main.c
@@ -1,45 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
 
 typedef struct mynumber{
-  unsigned long number;
-  int* tabnumber;
-} mystruct;
+  uint64_t number;
+  int tabnumber[1000];
+  int taille;
+   }mynumber;
 
-mystruct tab={0,NULL};
+uint64_t power(int n, int k){
+  uint64_t pow=1;
+  for(int i=0;i<k;i++)
+    pow*=n;
+  return pow;
+}
+
+void decalage(mynumber* tab, int rang){
+  int i=rang-1,mataille=tab->taille;
+  for (int j=mataille-1;j>=0;j--){
+    tab->tabnumber[i]=tab->tabnumber[j];
+    i--;
+  }
+  for(int j=0;j<(rang-mataille);j++) tab->tabnumber[j]=0;
+  tab->taille=rang;
   
-mystruct convert(unsigned long number, mystruct mynumber){
-  int valeur,taille=0;
-  mynumber.number=number;
-  mynumber.tabnumber=malloc(sizeof(int));
-  div_t division=div(number,2);
-  valeur=division.quot;
-  int i=0;
-  while (valeur!=0){
-    mynumber.tabnumber[i]=division.rem;
+}
+  
+mynumber* convert(int nombre, mynumber* tab){
+   tab=malloc(sizeof(mynumber));
+ 
+  tab->number=nombre;
+  
+    div_t division=div(nombre,2);
+    int valeur=division.quot;
+   int i=0,taille=0;
+   while (valeur!=0){
+    tab->tabnumber[i]=division.rem;
     division=div(valeur,2);
     valeur=division.quot;
     i++;
     taille++;
   }
-  mynumber.tabnumber[i]=division.rem;
+  tab->tabnumber[i]=division.rem;
   taille++;
-  int inter=0, k=taille;
+  tab->taille=taille;
+    int inter=0; int k=taille;
   for (int i=0;i<(taille/2);i++){
-    inter=mynumber.tabnumber[i];
-    mynumber.tabnumber[i]=mynumber.tabnumber[k-1];
-    mynumber.tabnumber[k-1]=inter;
+    inter=tab->tabnumber[i];
+    tab->tabnumber[i]=tab->tabnumber[k-1];
+    tab->tabnumber[k-1]=inter;
     k--;
-  }
-  printf("\n le nombre d'élément est: %d\n ",taille);
-  for (int j=0;j<taille;j++) printf("%d  ",mynumber.tabnumber[j]);
-  return mynumber;
+    }
+
+return tab;
 }
-    
-int main(){
+
+uint64_t addition(mynumber* struct1, mynumber* struct2){
+  if(struct1->taille>struct2->taille)
+    decalage(struct2,struct1->taille);
+   
+  else if (struct2->taille>struct1->taille)
+    decalage(struct1,struct2->taille);
+  int ret=0; int l=0; int* result=malloc(1000*sizeof(int));
+  for (int i=struct1->taille-1;i>=0;i--){
+    int tmp=struct1->tabnumber[i]+struct2->tabnumber[i]+ret;
+  if (tmp==2){
+    result[l]=0;
+    ret=1;
+    l++;
+  }
+  else if(tmp==3){
+    result[l]=1;
+    ret=1;
+    l++;
+  }
+  else {
+    result[l]=tmp;
+    l++;
+    ret=0;
+  }
+  }
+  if (ret==1) result[l]=1;
+  struct1->taille=l+1;
+  int k=l+1; uint64_t add=0;
+   for(int i=k;i>=0;i--)
+    add=add+result[i]*power(2,i);
+  return add;
+}
+
+int main(int argc,char** argv){
   
-  convert(14.0,tab);
+  uint64_t a=0; uint64_t b=0;
+  printf("donner les nombres\n");
+  scanf("%lu %lu",&a,&b);
+  mynumber* struct1=convert(a,struct1);
+  mynumber* struct2=convert(b,struct2);
+   uint64_t moi=addition(struct1,struct2);
+   printf("la somme vaut %lu \n",moi);
+  
   return 0;
 }
 
